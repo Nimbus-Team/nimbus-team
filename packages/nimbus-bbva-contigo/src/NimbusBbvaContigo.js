6c376b1d9de3afad737630600979944eea891dba
BlackE <elizabeth.ramescamilla@gmail.com>
2021-10-22 19:47:56 -0500

    front nimbus

--- packages/nimbus-bbva-contigo/src/NimbusBbvaContigo.js
+++ packages/nimbus-bbva-contigo/src/NimbusBbvaContigo.js
@@ -1,90 +1,95 @@
 import { LitElement, html, css } from 'lit';
 
-const logo = new URL('../assets/open-wc-logo.svg', import.meta.url).href;
-
 export class NimbusBbvaContigo extends LitElement {
   static get properties() {
     return {
       title: { type: String },
+      country : {type:String},
+      socket:{type:Object}
     };
   }
 
   static get styles() {
     return css`
-      :host {
-        min-height: 100vh;
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        justify-content: flex-start;
-        font-size: calc(10px + 2vmin);
-        color: #1a2b42;
-        max-width: 960px;
-        margin: 0 auto;
-        text-align: center;
-        background-color: var(--nimbus-bbva-contigo-background-color);
-      }
-
-      main {
-        flex-grow: 1;
-      }
-
-      .logo {
-        margin-top: 36px;
-        animation: app-logo-spin infinite 20s linear;
-      }
-
-      @keyframes app-logo-spin {
-        from {
-          transform: rotate(0deg);
-        }
-        to {
-          transform: rotate(360deg);
-        }
-      }
-
-      .app-footer {
-        font-size: calc(12px + 0.5vmin);
-        align-items: center;
-      }
-
-      .app-footer a {
-        margin-left: 5px;
-      }
+      
     `;
   }
 
   constructor() {
     super();
-    this.title = 'My app';
+    this.socket = io('http://localhost:5000');
+    this.socket.on("connect", () => {
+      console.log("client side socket connection established");
+    });
+    this.socket.on('tweet', data => {
+      console.log(data);
+    });
+    this.socket.on('suggestion', data => {
+      console.log('suggestion');
+      console.log(data);
+    });
+
+    this.title = 'BBVA Contigo - EMOP';
+
+  }
+
+
+
+  
+  changeCountryMX(){
+    
+    // this.socket.emit('request-suggestion','mx');
+
+  }
+  changeCountryES(){
+    // this.socket.emit('request-suggestion','es');
+  }
+
+  // setTweets(){
+  //   fetch('./25c354bf.json')
+  //   .then(response => response.json())
+  //   .then(data => {
+  //     let twitter = '';
+  //     data.forEach(item => {
+  //       let f = new Date(item.tweet.created_at);
+  //       twitter += `
+  //         <div class="tweet">
+  //           <div class="icon"></div>
+  //           <div class="tweetTexto">
+  //             <p class="texto">${item.tweet.text}</p>
+  //             <p class="fecha">${f.toLocaleDateString('en-US')}</p>
+  //           </div>
+  //         </div>`;
+  //     });
+  //     console.log(twitter);
+  //     document.getElementById('tweetContainer').innerHTML = twitter;
+      
+  //   });
+  // }
+
+  createRenderRoot() {
+    return this;
   }
 
   render() {
     return html`
       <main>
-        <div class="logo"><img alt="open-wc logo" src=${logo} /></div>
-        <h1>${this.title}</h1>
-
-        <p>Edit <code>src/NimbusBbvaContigo.js</code> and save to reload.</p>
-        <a
-          class="app-link"
-          href="https://open-wc.org/guides/developing-components/code-examples/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Code examples
-        </a>
+        <div class="container"><h1>${this.title}</h1></div>
+        <div class="container" id="btnsContainer">
+          <button class="active" @click="${this.changeCountryMX}">MEX</button>
+          <button @click="${this.changeCountryES}">ESP</button>
+        </div>
+        <div class="container" id="firstBlock">
+          <div class="mapContainer">
+            <div class="d3_globe" id="d3_globe"></div>
+          </div>
+          <div class="twitterContainer">
+            <h2>Tweets</h2>
+            <div class="tweetContainer" id="tweetContainer">
+            </div>
+          </div>
+        </div>
       </main>
-
-      <p class="app-footer">
-        🚽 Made with love by
-        <a
-          target="_blank"
-          rel="noopener noreferrer"
-          href="https://github.com/open-wc"
-          >open-wc</a
-        >.
-      </p>
     `;
   }
 }
