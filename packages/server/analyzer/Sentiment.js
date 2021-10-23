c5a9c66285f7aa2a22bf26bcbd84b2d1d85580f5
armando <armando.lezama.psic@gmail.com>
2021-10-23 13:42:57 -0500

    fix(js)/ added filters to initial tokens and added truncate trending

--- packages/server/analyzer/Sentiment.js
+++ packages/server/analyzer/Sentiment.js
@@ -1,7 +1,11 @@
 const Sentiment = require('sentiment');
 const sentiment = new Sentiment();
 const trainedData = require('./training.js');
+const excludedWords = require('./excluded-words');
 
 module.exports = function(text) {
-    return sentiment.analyze(text, trainedData);
+    const cleanText = text.split(' ').filter(word => {
+        return !word.includes('@') && word.length > 1 && !excludedWords.includes(word) && numbersRegex.test(word);
+    }).join(' ');
+    return sentiment.analyze(cleanText, trainedData);
 }
