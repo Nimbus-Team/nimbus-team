e08f4088770eb97cb46d9c91d1e07c8af2667461
Alfonso Rios <manuel.rios@ironbit.com.mx>
2021-10-23 11:18:36 -0500

    fix cors

--- packages/server/app.js
+++ packages/server/app.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
 const Twitter = require('twitter-v2');
 
 const client = new Twitter({
@@ -7,6 +8,7 @@ const client = new Twitter({
 });
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 
