a0460fe94a6f7e041efb06f35b9ded802f009a05
Aarón Hinojosa <aaron.hmaya@gmail.com>
2021-10-24 02:55:28 -0500

    fucking mongo

--- packages/server/db/MongoAccess.js
+++ packages/server/db/MongoAccess.js
@@ -2,7 +2,7 @@ const { MongoClient } = require('mongodb');
 
 class MongoAccess {
     constructor() {
-        this.__uri = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@cluster0.${process.env.MONGO_CLUSTER}.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+        this.__uri = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@${process.env.MONGO_CLUSTER_NAME}.${process.env.MONGO_CLUSTER}.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`;
         this.mongo = new MongoClient(this.__uri);
     }
 
